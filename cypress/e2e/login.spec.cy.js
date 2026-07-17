6410e1002c191ceb28df34094f783e07a5d3634f
Daniel Murta <danielmurta2011@gmail.com>
2026-07-17 17:06:45 -0300

    cypressConfig

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,7 +11,7 @@ describe('login - sucess', () => {
   }
   
   it ('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type (userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type (userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click ()
@@ -20,7 +20,7 @@ describe('login - sucess', () => {
 
   })
   it ('login - fail', () => {
-      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type (userData.userFail.username)
     cy.get(selectorsList.passwordField).type (userData.userFail.password)
     cy.get(selectorsList.loginButton).click ()
